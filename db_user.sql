528aaf77d0eaaef0e9eff9c3b9755042f0ea3c8f
Sergio Wicramachine <127046969+wicra@users.noreply.github.com>
2025-01-31 19:33:00 +0100

    Maj db

--- db_user.sql
+++ db_user.sql
@@ -14,16 +14,16 @@ INSERT INTO type_user (name) VALUES
 ('Admin');
 
 
+
 -- Création de la table users
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    mdp VARCHAR(64) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- id auto-incrémenté
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    mdp VARCHAR(64) NOT NULL, -- SHA-256 hash du mot de passe
     is_admin BOOLEAN DEFAULT FALSE,
     type_user_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_sign TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (type_user_id) REFERENCES type_user(id)
 );
-
